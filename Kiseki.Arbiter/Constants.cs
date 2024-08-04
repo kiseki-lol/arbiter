b5478e654e8711c83f7520c9d37c57e90aaa577d
d9mz <101074954+d9mz@users.noreply.github.com>
2024-08-04 03:45:40 -0700

    chore: bloxblox.xyz => kiseki.local

--- Kiseki.Arbiter/Constants.cs
+++ Kiseki.Arbiter/Constants.cs
@@ -5,7 +5,7 @@ public static class Constants
     public const string PROJECT_NAME = "Kiseki";
 
 #if DEBUG
-    public const string BASE_URL = "bloxblox.xyz";
+    public const string BASE_URL = "kiseki.local";
 #else
     public const string BASE_URL = "kiseki.lol";
 #endif
